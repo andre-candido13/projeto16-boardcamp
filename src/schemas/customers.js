5133cb4eed3f32f344514a1bddce4e6bba3b764d
Andre <andre.eng1313@gmail.com>
2023-02-11 11:13:59 -0300

    tentando acertar no avaliador em clientes

--- src/schemas/customers.js
+++ src/schemas/customers.js
@@ -4,13 +4,10 @@ import joi from "joi"
 
 export const customersJoi = joi.object({
 
-    name: joi.string().required(),
-    cpf: joi.string()
-    .pattern(/^[0-9]{11}$/)
-    .required(),
-  phone: joi.string()
-    .pattern(/^[0-9]{10,11}$/)
-    .required(),
-  birthday: joi.string().pattern(/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/),
-})
+  name: joi.string().empty().required(),
+  phone: joi.string().min(10).max(11).required(),
+  cpf: joi.string().pattern(/^[0-9]+$/).length(11).required(),
+  birthday: joi.date().max('now').required()
+  })
+
 
