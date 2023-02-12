35b90defdbdb328bb332c2457869fc0a9c9314f6
Andre <andre.eng1313@gmail.com>
2023-02-12 15:41:51 -0300

    acertando erro no delete

--- src/controllers/rentals.js
+++ src/controllers/rentals.js
@@ -65,53 +65,52 @@ return res.status(201).send("OK")
 
 export async function finalizarAluguel (req, res) {
 
+  const dataAtual = new Date();
+  const retornoAluguel = dataAtual.toISOString().split("T")[0]
+  const { id } = req.params;
+  
+
   try {
-    const { id } = req.params
-    
-    const dataAtual = new Date();
-    const retornoAluguel = dataAtual.toISOString().split("T")[0];
-    
-    
-    const aluguelExistente = await db.query(`SELECT * FROM rentals WHERE id=$1`,
-    ([id]))
-    
-    if (aluguelExistente.rows.length === 0) {
-        return res.status(400).send("Aluguel não encontrado!")
+    const aluguel = await db.query('SELECT * FROM rentals WHERE id = $1',
+      [id]
+    );
+    const aluguelExiste = aluguel.rowCount !== 0
+    if (!aluguelExiste) {
+      return res.status(404).send("Aluguel não encontrado")
     }
-    
-    const aluguel = aluguelExistente.rows[0]
-    
-    if (aluguel.returnDate !== null) {
-        return res.status(400).send("Aluguel disponivel")
-    }
-    
-    const dataAluguel = new Date(aluguel.dataAluguel);
-    const diasAlugados = aluguel.diasAlugados;
-    const devolucao = new Date(retornoAluguel);
-    const tempoAlugado = Math.abs(devolucao.getTime() - dataAluguel.getTime());
-    const tempoGasto = Math.ceil(tempoAlugado / (1000 * 3600 * 24));
-    const atraso = tempoGasto- diasAlugados;
-    let delayFee = 0;
-    if (atraso > 0) {
-      const game = await db.query('SELECT * FROM games WHERE id = $1', [
-        aluguel.gameId,
-      ]);
-      
-      const { pricePerDay } = game.rows[0]
-      delayFee = atraso * pricePerDay;
-    }
-    
-    const update = await db.query(`UPDATE rentals SET "returnDate" = $1, "delayFee" = $2 WHERE id = $3 `,
-    ([retornoAluguel, delayFee, id]))
-    
-    return res.status(200).send("OK")
-    
-    } catch (err) {
-        res.status(500).send(err.message)
-    }
-    
+    let verif = aluguel.rows[0]
+
+    const retorno = verif.returnDate
+    if (retorno !== null) {
+      return res.status(400).send("Aluguel disponivel")
     }
 
+    const dataAluguel = new Date(verif.dataAluguel)
+    const diasAlugados = verif.diasAlugados
+    const dataRetorno = new Date(retornoAluguel)
+    const tempo = Math.abs(dataRetorno.getTime() - dataAluguel.getTime())
+    const dia = Math.ceil(tempo / (1000 * 3600 * 24));
+    const diasAtrasados = dia - diasAlugados
+    let delayFee = 0
+    if (diasAtrasados > 0) {
+      const game = await db.query('SELECT * FROM games WHERE id = $1', [ verif.gameId, ]);
+      
+      const { pricePerDay } = game.rows[0];
+
+
+      delayFee = diasAtrasados * pricePerDay;
+    }
+    
+    await db.query(
+      'UPDATE rentals SET "returnDate" = $1, "delayFee" = $2 WHERE id = $3',
+      [retornoAluguel, delayFee, id] 
+    )
+
+    res.status(200).send("OK")
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
 
     export async function deletarAluguel (req, res) {
 
@@ -126,21 +125,14 @@ export async function finalizarAluguel (req, res) {
           return res.status(404).send("Aluguel não encontrado")
         }
 
-        if (selec.rows[0].returnDate) {
-          return res.status(400).send("Jogo consta alugado")
+        if (!selec.rows[0].returnDate) {
+          return res.status(400).send("Não finalizado")
 
         }
 
         const deleteAluguel = await db.query(`DELETE FROM rentals WHERE id= $1`,
         ([id]))
         res.status(200).send("DELETADO")
-
-
-
-
-
-
-
 
 
       } catch (err) {
